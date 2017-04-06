bbf4917afe77efec8c13a34d56dac1eaab855d31
Eric Barbour <atelic@protonmail.com>
2017-04-06 10:48:17 -0400

    Reorganize groups. Something is off with show.js

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -2,15 +2,4 @@ import Ember from 'ember';
 import CurrentUserMixin from '../mixins/current-user';
 
 export default Ember.Route.extend(CurrentUserMixin, {
-  model() {
-    return {
-      users: this.store.findAll('user'),
-      groups: this.store.findAll('group'),
-      projects: this.store.query('node', {
-        filter: {
-          contributors: this.get('user').id
-        }
-      })
-    };
-  }
 });
--- app/routes/groups/index.js
+++ app/routes/groups/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import CurrentUserMixin from '../../mixins/current-user';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(CurrentUserMixin, {
+  model() {
+    return {
+      users: this.store.findAll('user'),
+      groups: this.store.findAll('group'),
+      projects: this.store.query('node', {
+        filter: {
+          contributors: this.get('user').id
+        }
+      })
+    };
+  }
 });
