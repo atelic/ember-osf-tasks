f6641c68f69f3d1ccf7333cb1efda388da744818
Eric Barbour <atelic@protonmail.com>
2017-04-11 10:06:25 -0400

    Fix inline-user for group/show and profile page.

--- app/components/inline-user.js
+++ app/components/inline-user.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  computedUser: function () {
+    const id = this.get('user');
+    return this.get('store').findRecord('user', id);
+  }.property('user'),
+
   tagName: 'span'
 });
