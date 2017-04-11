de19fc94401ae046c82370e7f04669bd244f9d9b
Eric Barbour <atelic@protonmail.com>
2017-04-11 12:14:11 -0400

    Add ability to make a project public from group view
    Closes #5 (for now)

--- app/components/inline-project.js
+++ app/components/inline-project.js
@@ -16,5 +16,17 @@ export default Ember.Component.extend({
     return this.get('store').findRecord('node', id);
   }.property('project'),
 
+  actions: {
+    makePublic() {
+      if (confirm('Are you sure? This will make any sensitive data publicly visible')) {
+        const id = this.get('project');
+        this.get('store').findRecord('node', id).then(node => {
+          node.set('public', true);
+          node.save();
+        });
+      }
+    }
+  },
+
   tagName: 'span'
 });
