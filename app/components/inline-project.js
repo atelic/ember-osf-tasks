5e95417d844b46be9c78ab4b02c0aea924fe45a6
Eric Barbour <atelic@protonmail.com>
2017-04-11 11:57:11 -0400

    Link to projects and users

--- app/components/inline-project.js
+++ app/components/inline-project.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
+import config from 'ember-get-config';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+
+  host: config.OSF.url,
+
+  link: function() {
+    const id = this.get('project');
+    return this.get('host') + id;
+  }.property('project'),
 
   computedProject: function () {
     const id = this.get('project');
--- app/components/inline-user.js
+++ app/components/inline-user.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
+import config from 'ember-get-config';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+
+  host: config.OSF.url,
+
+  link: function() {
+    const id = this.get('user');
+    return this.get('host') + id;
+  }.property('user'),
 
   computedUser: function () {
     const id = this.get('user');
