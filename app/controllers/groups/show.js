75071a4e20dc389fe7e219310b69e8cf63918d22
Eric Barbour <atelic@protonmail.com>
2017-04-13 10:59:34 -0400

    Improve look and use of user and node search field
    Closes #3

--- app/controllers/groups/show.js
+++ app/controllers/groups/show.js
@@ -23,89 +23,98 @@ export default Ember.Controller.extend({
       } else {
         this.set('userSearchResults', []);
       }
-    }
-  },
-  projectSearch(query) {
-    if (query) {
-      const results = this.get('store').query('node', {
-        filter: {
-          title: query
-        }
-      });
-      this.set('projectSearchResults', results);
-    } else {
+    },
+    clearUserSearch() {
+      this.set('userSearchQuery', '');
+      this.set('userSearchResults', []);
+      this.toggleProperty('showAddUser');
+    },
+    projectSearch(query) {
+      if (query) {
+        const results = this.get('store').query('node', {
+          filter: {
+            title: query
+          }
+        });
+        this.set('projectSearchResults', results);
+      } else {
+        this.set('projectSearchResults', []);
+      }
+    },
+    clearProjectSearch() {
+      this.set('projectSearchQuery', '');
       this.set('projectSearchResults', []);
-    }
-  },
-  saveGroup(newName) {
-    const groupId = this.get('model').id;
-    this.store.findRecord('group', groupId).then(group => {
-      group.set('name', newName);
-      group.save();
-    });
-
-  },
-  deleteGroup() {
-    if (confirm('This will delete this entire group. Are you sure?')) {
+      this.toggleProperty('showAddProject');
+    },
+    saveGroup(newName) {
       const groupId = this.get('model').id;
       this.store.findRecord('group', groupId).then(group => {
-        group.destroyRecord();
-        this.transitionToRoute('groups');
+        group.set('name', newName);
+        group.save();
+      });
+
+    },
+    deleteGroup() {
+      if (confirm('This will delete this entire group. Are you sure?')) {
+        const groupId = this.get('model').id;
+        this.store.findRecord('group', groupId).then(group => {
+          group.destroyRecord();
+          this.transitionToRoute('groups');
+        });
+      }
+    },
+    toggleAddUser() {
+      this.toggleProperty('showAddUser');
+    },
+    addUser(user) {
+      this.toggleProperty('showAddUser');
+      const groupId = this.get('model').id;
+      this.store.findRecord('group', groupId).then(group => {
+        const users = group.get('users');
+        if (users) {
+          group.set('users', users.concat([user]));
+        } else {
+          group.set('users', [user]);
+        }
+        group.save();
+      });
+    },
+    removeUser(user) {
+      const groupId = this.get('model').id;
+      this.store.findRecord('group', groupId).then(group => {
+        let users = group.get('users');
+        const idx = users.indexOf(user);
+        users.splice(idx, 1);
+        group.set('users', users);
+        group.save();
+      });
+    },
+    toggleAddProject() {
+      this.toggleProperty('showAddProject');
+    },
+    addProject(proj) {
+      this.toggleProperty('showAddProject');
+      this.set('projectSearchResults', []);
+      const groupId = this.get('model').id;
+      this.store.findRecord('group', groupId).then(group => {
+        const projects = group.get('projects');
+        if (projects) {
+          group.set('projects', projects.concat([proj]));
+        } else {
+          group.set('projects', [proj]);
+        }
+        group.save();
+      });
+    },
+    removeProject(proj) {
+      const groupId = this.get('model').id;
+      this.store.findRecord('group', groupId).then(group => {
+        let projects = group.get('projects');
+        const idx = projects.indexOf(proj);
+        projects.splice(idx, 1);
+        group.set('projects', projects);
+        group.save();
       });
     }
-  },
-  toggleAddUser() {
-    this.toggleProperty('showAddUser');
-  },
-  addUser(user) {
-    this.toggleProperty('showAddUser');
-    const groupId = this.get('model').id;
-    this.store.findRecord('group', groupId).then(group => {
-      const users = group.get('users');
-      if (users) {
-        group.set('users', users.concat([user]));
-      } else {
-        group.set('users', [user]);
-      }
-      group.save();
-    });
-  },
-  removeUser(user) {
-    const groupId = this.get('model').id;
-    this.store.findRecord('group', groupId).then(group => {
-      let users = group.get('users');
-      const idx = users.indexOf(user);
-      users.splice(idx, 1);
-      group.set('users', users);
-      group.save();
-    });
-  },
-  toggleAddProject() {
-    this.toggleProperty('showAddProject');
-  },
-  addProject(proj) {
-    this.toggleProperty('showAddProject');
-    this.set('projectSearchResults', []);
-    const groupId = this.get('model').id;
-    this.store.findRecord('group', groupId).then(group => {
-      const projects = group.get('projects');
-      if (projects) {
-        group.set('projects', projects.concat([proj]));
-      } else {
-        group.set('projects', [proj]);
-      }
-      group.save();
-    });
-  },
-  removeProject(proj) {
-    const groupId = this.get('model').id;
-    this.store.findRecord('group', groupId).then(group => {
-      let projects = group.get('projects');
-      const idx = projects.indexOf(proj);
-      projects.splice(idx, 1);
-      group.set('projects', projects);
-      group.save();
-    });
   }
-}
 });
