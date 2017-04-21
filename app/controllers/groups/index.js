8445f70ec45a95f2a093afc91c7c41faa827137b
Eric Barbour <atelic@protonmail.com>
2017-04-21 11:10:22 -0400

    Add quick add to group for users on group list

--- app/controllers/groups/index.js
+++ app/controllers/groups/index.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   showInput: false,
+  showAdd: false,
 
   actions: {
     toggleShow() {
       this.toggleProperty('showInput');
+    },
+    toggleAddUser() {
+      this.toggleProperty('showAdd');
     },
     addItem(name) {
       this.toggleProperty('showInput');
@@ -16,6 +20,21 @@ export default Ember.Controller.extend({
       };
       const group = this.store.createRecord('group', newGroup);
       group.save();
+    },
+    addUser(user, group) {
+      this.toggleProperty('showAdd');
+      const groupId = group.id;
+      this.store.findRecord('group', groupId).then(group => {
+        const users = group.get('users');
+        if (users) {
+          if(!users.includes(user.id))
+            group.set('users', users.concat([user.id]));
+        } else {
+          group.set('users', [user]);
+        }
+        group.save();
+      });
+
     }
   }
 });
